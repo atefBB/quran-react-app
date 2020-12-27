68df213247ea8dd96fa334a1a0de8b4adaaa3315
Atef Ben Ali <atef.bettaib@gmail.com>
2020-12-27 14:12:32 +0100

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+	      بسم الله الرحمن الرحيم
+      </header>
+    </div>
+  );
+}
