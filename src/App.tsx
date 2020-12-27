c7cade2aa1b22bf9748c4d13de2320fba2e30618
Atef Ben Ali <atef.bettaib@gmail.com>
2020-12-27 20:29:39 +0100

    fix(ui): add `quran pages` in `app` component using `swiper` package

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,41 @@
-import React from 'react';
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
 
-import './App.css';
+import "swiper/swiper.scss";
+
+import "./assets/css/app.css";
 
 export default function App() {
+  const [currentPage, setCurrentPage] = React.useState<number>(0);
+
+  const slides = Array.from({ length: 611 }).map(
+    (el, index) => `Slide ${index + 1}`
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-	      بسم الله الرحمن الرحيم
-      </header>
+      <section>
+        <Swiper
+          spaceBetween={0}
+          slidesPerView={1}
+          onSlideChange={() => setCurrentPage(currentPage + 1)}
+          onSwiper={(swiper: any) => {
+            setCurrentPage(currentPage + 1);
+            console.log(currentPage);
+          }}
+        >
+          {slides.map((el: any, index: number) => {
+            return (
+              <SwiperSlide key={el} virtualIndex={index}>
+                <img
+                  src={`/assets/images/quran/${index}.png`}
+                  alt={`page ${currentPage}`}
+                />
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>
+      </section>
     </div>
   );
 }
