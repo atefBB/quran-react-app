2a418766ff4d767b9523741f837d387d83a89ee0
Aissaoui Ahmed <20658625+Aissaoui-Ahmed@users.noreply.github.com>
2021-01-02 03:54:34 +0100

    Update App.test.tsx

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('image alt', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  screen.getAllByAltText(/page/i).map((el) => {
+    expect(el).toBeInTheDocument()
+  })
 });
